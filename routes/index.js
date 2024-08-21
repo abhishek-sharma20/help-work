e9b6663e60fef3a93a319479755cc60b452b94f5
abhishek-sharma20 <118713171+abhishek-sharma20@users.noreply.github.com>
2024-08-21 20:46:40 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import user from "./user.js"; 
 import laborsType from "./laborsType.js";
 import gig from "./gig.js";
-import conversation from "./conversation.js";
+import conversation from "./conversation.js"; 
 import message from "./message.js";
 import project from "./project.js";
 
